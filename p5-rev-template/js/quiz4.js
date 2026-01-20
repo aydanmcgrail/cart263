54bc656001757b6e907382c63575c6a2d85132d9
aydanmcgrail <aydanmc2323@gmail.com>
2026-01-19 22:28:48 -0800

    Update quiz4.js

--- p5-rev-template/js/quiz4.js
+++ p5-rev-template/js/quiz4.js
@@ -5,32 +5,37 @@ let square1 = {
     x: 0,
     y: 0,
     height: 800,
-    fill: [200, 30, 70],
+    width: 400,
+    fill: "rgb(189, 221, 95)",
     fills: {
-        overlap: [169, 230, 160],
-        noOverlap: [200, 30, 70],
+        overlap: "rgb(209, 83, 33)",
+        noOverlap: "rgb(189, 221, 95)",
     }
 }
 
 let square2 = {
     x: 400,
     y: 0,
-    fill: [10, 150, 180],
+    width: 400,
+    fill: "rgb(10, 150, 180)",
     fills: {
-        overlap: [66, 186, 162],
-        noOverlap: [10, 150, 180],
+        overlap: "rgb(130, 103, 218)",
+        noOverlap: "rgb(10, 150, 180)",
     }
 }
 
 let square3 = {
     x: 800,
     y: 0,
-    fill: [100, 190, 230],
+    fill: [100, 190, 30],
     fills: {
         overlap: [36, 78, 120],
         noOverlap: [100, 190, 230],
     }
 }
+
+let squareWidth = 400;
+let squareHeight = 800;
 
 function setup() {
     console.log("go")
@@ -44,12 +49,11 @@ function draw() {
 
     noStroke();
 
-    let squareWidth = 400;
-    let squareHeight = 800;
+
 
     push();
-    fill(square1.fill);
-    rect(square1.x, square1.y, squareWidth, square1.height);
+    fill(square1.fills.noOverlap.r, square1.fills.noOverlap.g, square1.fills.noOverlap.b);
+    rect(square1.x, square1.y, square1.width, square1.height);
     pop();
 
     push();
@@ -62,16 +66,19 @@ function draw() {
     rect(square3.x, square3.y, squareWidth, squareHeight);
     pop();
 
+    //textSize(28);
+    //text(distSquare1, 200, 200);
+
 }
 
 function checkInput() {
     const distSquare1 = dist(mouseX, mouseY, square1.x, square1.y);
-    const overlapSquare1 = distSquare1 < square1.height / 2 && squareWidth / 2;
+    const overlapSquare1 = distSquare1 < square1.height;
 
     if (overlapSquare1) {
         square1.y += 1;
         square1.fill = square1.fills.overlap;
-    }
+    } else { square1.fill = square1.fills.noOverlap; }
     //else { square1.fills.noOverlap };
 
     //*const distSquare2 = dist(mouseX, mouseY, square2.x, square2.y);
