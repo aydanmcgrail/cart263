b6d1e4c276342d3e94e0883aed630fda48be5c01
aydanmcgrail <aydanmc2323@gmail.com>
2026-03-23 10:37:35 -0400

    task 6

---
+++ CART263-TASK-6/js/FreeStyleObj.js
@@ -0,0 +1,41 @@
+class FreeStyleObj {
+    constructor(x, y, length, f_color, s_color,context) {
+      // We write instructions to set up a Flower here
+      // Position and size information
+      this.x = x;
+      this.y = y;
+      this.fill_color = f_color;
+      this.stroke_color = s_color;
+      this.theta = 0;
+      this.length = length;
+      this.yOffset = 20;
+      this.angularSpeed = .07;
+      this.context =context;
+
+    }
+  
+    display() {
+      this.theta =0; //reset everytime
+      this.context.fillStyle = this.fill_color; // change the color we are using
+      this.context.strokeStyle = this.stroke_color; // change the color we are using
+      this.context.beginPath();
+      this.context.moveTo(this.x,this.y)
+      for(let i =this.x; i< this.x+this.length; i++){
+      this.context.lineTo(i,(Math.sin(this.theta)*5)+this.y)
+      this.context.lineTo(i,(Math.sin(this.theta)*5)+this.y+this.yOffset)
+      this.theta+=this.angularSpeed;
+      }
+      this.context.stroke(); //set the stroke
+    }
+
+    update(volume){
+        //update freestyle - move left and right based on volume
+        const threshold = 40;
+        
+        if (volume > threshold) {
+          // Move left and right based on volume
+          this.x += Math.sin(Date.now() * 0.005) * (volume * 0.1);
+        }
+    }
+  }
+  
---
+++ CART263-TASK-6/js/RectangularObj.js
@@ -0,0 +1,59 @@
+class RectangularObj {
+  constructor(x, y, w, h, f_color, s_color, context) {
+    // We write instructions to set up a Flower here
+    // Position and size information
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.fill_color = f_color;
+    this.stroke_color = s_color;
+    this.startAngle = 0;
+    this.endAngle = Math.PI * 2; //full rotation
+    this.context = context;
+    this.rotation = 0; // New property for rotation
+  }
+
+  display() {
+    // Save the context state
+    this.context.save();
+    
+    // Translate to the center of the rectangle
+    this.context.translate(this.x + this.width / 2, this.y + this.height / 2);
+    
+    // Rotate
+    this.context.rotate(this.rotation);
+    
+    // Draw the rectangle centered at the origin
+    this.context.fillStyle = this.fill_color; // change the color we are using
+    this.context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+    this.context.strokeStyle = this.stroke_color; // change the color we are using
+    this.context.lineWidth = 2; //change stroke
+    this.context.strokeRect(-this.width / 2, -this.height / 2, this.width, this.height);
+    
+    // Restore the context state
+    this.context.restore();
+  }
+
+  update(volume) {
+    const threshold = 40;
+
+    if (volume > threshold) {
+      // ROTATE based on volume
+      this.rotation += volume * 0.02; // Increases rotation speed with volume
+      
+      // MOVE based on volume
+      this.y -= volume * 0.1;
+
+      // RANDOM COLOR
+      this.fill = `rgb(
+      ${Math.random() * 255},
+      ${Math.random() * 255},
+      ${Math.random() * 255}
+    )`;
+    } else {
+      // gravity fallback
+      this.y += 2;
+    }
+  }
+}
